2008c99e471259584bc05092d1b876da96ef8ef0
JP Henderson <jpcoperstar@icloud.com>
2025-07-19 09:05:31 -0700

    Initial Commit

---
+++ LandShip/LandShipApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandShipApp.swift
+//  LandShip
+//
+//  Created by JP on 7/19/25.
+//
+
+import SwiftUI
+
+@main
+struct LandShipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
